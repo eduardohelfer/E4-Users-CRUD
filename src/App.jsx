5001577ea982c536c05bd7e42c48976b9a44711e
Eduardo Helfer <eduardohelfer@gmx.net>
2022-10-18 01:23:40 -0500

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import './App.css'
 import FormUsers from './components/FormUsers'
 import UserCard from './components/UserCard'
 
-const baseURL = 'http://144.126.218.162:9000'
+const baseURL = 'https://144.126.218.162:9000'
 // const baseURL = 'https://users-crud1.herokuapp.com'
 
 function App() {
